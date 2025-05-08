2275c8a8548b0ef9afdb731c024a7d034d65d119
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-05-08 07:49:16 -0300

    main

--- colors.py
+++ colors.py
@@ -4,3 +4,5 @@ co2= "#696969" # DimGray
 co3= "#C0C0C0" # Silver 
 co4= "#D3D3D3" # LightGray
 co5= "#FFFFFF" # White
+co6= "#000000" # Black
+co7= "#FF0000" # Red
--- main.py
+++ main.py
@@ -18,8 +18,20 @@ pos_y = (altura_tela - altura_root)//2
 # Definir geometria da janela (LxA+X+Y)
 root.geometry(f"{largura_root}x{altura_root}+{pos_x}+{pos_y}")
 
+###############---------FRAME------##################################################################################
+frame_cima = Frame(root, width=600, height=50, bg=co1, relief='flat')
+frame_cima.grid(row=0, column=0, padx=0, pady=0, sticky=NSEW)
+
+frame_baixo = Frame(root, width=600, height=350, bg=co1, relief='flat')
+frame_baixo.grid(row=1, column=0, padx=0, pady=0, sticky=NSEW)
 
 
+
+#################---------TITULO------##################################################################################
+l_titulo= Label(frame_cima, text="Controle de Rotas e Ganhos", anchor=CENTER, font=('Ivy 13 bold'), bg=co6, fg=co0)
+l_titulo.place(x=0, y=0, relwidth=1, relheight=1)
+
+#################---------BOTÕES------##################################################################################
 
 
 
